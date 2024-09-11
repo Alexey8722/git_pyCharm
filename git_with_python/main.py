591cede191515f51835dbf221275d911ac393a32
lexa8722 <lexa8722@bk.ru>
2024-09-11 23:47:31 +0300

    add pytest

--- git_with_python/main.py
+++ git_with_python/main.py
@@ -1,14 +1,6 @@
+import pytest
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pytest.main()
 
-def minus(a,b):
-    return a+b
-print(minus(5,3))
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
